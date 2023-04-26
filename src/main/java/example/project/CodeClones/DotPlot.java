a176a7fd60ee0df3d572c771073da7ac053e5d20
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-04-26 14:36:45 +0100

    Add task 5

--- src/main/java/example/project/CodeClones/DotPlot.java
+++ src/main/java/example/project/CodeClones/DotPlot.java
@@ -4,6 +4,23 @@ public class DotPlot {
 
     public static void printDotPlot(char[] sequence1, char[] sequence2) {
         // TODO: complete this function to print the dot plot of two string arrays
-    }
 
+        for (char charcter : sequence2){
+            System.out.print("\t" + charcter);
+        }
+
+        System.out.print("\t\n");
+
+        for (char charcterI : sequence1) {
+            System.out.print(charcterI);
+            for (char charcterJ : sequence2) {
+                if (charcterI == charcterJ) {
+                    System.out.print("\t" + "+");
+                } else {
+                    System.out.print("\t" + " ");
+                }
+            }
+            System.out.print("\t\n");
+        }
+    }
 }
