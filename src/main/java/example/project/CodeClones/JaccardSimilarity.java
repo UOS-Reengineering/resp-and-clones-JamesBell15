29b0ac758b4dc54473d55d9e8156807d14e2179b
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-04-26 14:06:11 +0100

    Add task 4

--- src/main/java/example/project/CodeClones/JaccardSimilarity.java
+++ src/main/java/example/project/CodeClones/JaccardSimilarity.java
@@ -6,8 +6,15 @@ import java.util.Set;
 public class JaccardSimilarity {
 
     public static double computeJaccardSimilarity(Set<String> set1, Set<String> set2) {
-        // TODO: implement this function to compute the Jaccard similarity between two string arrays
-        return -1.0;
-    }
+        Set<String> intersectionResult = new HashSet<>(set1);
+        Set<String> unionResult = new HashSet<>(set1);
 
+        intersectionResult.retainAll(set2);
+        unionResult.addAll(set2);
+
+        double intersectionSize = intersectionResult.size();
+        double unionSize = unionResult.size();
+
+        return (double) (intersectionSize/unionSize);
+    }
 }
